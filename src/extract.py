aa1ca7c2f23ad645933cad21e29d7e6f630538a6
AffandraF <affandraf@gmail.com>
2025-09-08 20:12:49 +0700

    fix delta partition

--- src/extract.py
+++ src/extract.py
@@ -1,56 +1,63 @@
-from pyspark.sql.functions import col, year, month, lit
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType, LongType
+from pyspark.sql.functions import col, year, month, lit, to_date
+from utils.delta_schema import stock_schema
 import os
 import glob
 from utils.spark_builder import init_spark
 
-stock_schema = StructType([
-    StructField("date", StringType(), True),
-    StructField("ticker", StringType(), True),
-    StructField("open", DoubleType(), True),
-    StructField("high", DoubleType(), True),
-    StructField("low", DoubleType(), True),
-    StructField("close", DoubleType(), True),
-    StructField("volume", LongType(), True),
-])
-
 def load_historical_csv(spark, csv_path, output_path):
+
+    print(f"Processing {csv_path}...")
 
     ticker = os.path.basename(csv_path).split("_")[0] 
     
+    schema = stock_schema()
     df = (
-        spark.read.schema(stock_schema)
+        spark.read.schema(schema)
         .option("header", True)
         .csv(csv_path)
     )
 
+    for c in df.columns:
+        df = df.withColumnRenamed(c, c.lower())
+
     df = (
         df.withColumn("ticker", lit(ticker))
-          .withColumn("date", col("date").cast("date"))
+          .withColumn("date", to_date("date", "yyyy-MM-dd"))
           .withColumn("year", year(col("date")))
           .withColumn("month", month(col("date")))
     )
 
+    # Write to Delta Lake (MinIO/S3)
     (
         df.write
         .format("delta")
         .mode("append")
-        .partitionBy("ticker", "year", "month")
+        .partitionBy("year", "month")
         .save(output_path)
     )
 
     print(f"✅ Historical data for {ticker} saved to {output_path}")
 
 def extract_historical_flow(
-    csv_dir: str = "data/raw/",
-    output_path: str = "s3a://stock-data/raw/"
+    csv_dir: str = "/opt/spark-data/raw/",
+    output_path: str = "s3a://stock-data-lake/raw/"
 ):
-    spark = init_spark("StockExtract")
+    try:
+        spark = init_spark("StockExtract")
 
-    for csv_path in glob.glob(os.path.join(csv_dir, "*_history.csv")):
-        load_historical_csv(spark, csv_path, output_path)
+        print("Starting historical data extraction...")
+        for csv_path in glob.glob(os.path.join(csv_dir, "*_history.csv")):
+            load_historical_csv(spark, csv_path, output_path)
 
-    spark.stop()
+        print("✅ Extraction completed successfully")
+
+    except Exception as e:
+        print(f"Error during extraction: {e}")
+        raise
+
+    finally:
+        spark.stop()
+        print("Spark session stopped")
 
 if __name__ == "__main__":
     extract_historical_flow()
