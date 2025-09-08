5db21318035d82dc172d4d501c81b72b8eedb614
AffandraF <affandraf@gmail.com>
2025-09-08 20:14:52 +0700

    fix load data

--- src/transform_load.py
+++ src/transform_load.py
@@ -2,22 +2,30 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql.window import Window
+from pyspark.sql import DataFrame
+from delta.tables import DeltaTable
 from pyspark.sql.utils import AnalysisException
 from utils.spark_builder import init_spark
+import psycopg2
+import os
+
+def extract_raw(spark, raw_path: str):
+    df = spark.read.format("delta").load(raw_path)
+    print(f"✅ Extract raw data from {raw_path}")
+    return df
 
 def transform_data(df):
-    """Step 3: Transform with technical indicators."""
-    windowSpec = Window.partitionBy("ticker").orderBy("date") if "ticker" in df.columns else Window.orderBy("date")
+    windowSpec = Window.partitionBy("ticker").orderBy("date")
 
-    print("📊 Starting technical indicators calculation...")
+    print("Starting technical indicators calculation...")
 
     # SMA
-    print("➡️ Calculating SMA (5 & 20)")
+    print("Calculating SMA (5 & 20)")
     df = df.withColumn("SMA_5", F.avg("close").over(windowSpec.rowsBetween(-4, 0)))
     df = df.withColumn("SMA_20", F.avg("close").over(windowSpec.rowsBetween(-19, 0)))
 
     # EMA (approximation with lag)
-    print("➡️ Calculating EMA (12)")
+    print("Calculating EMA (12)")
     alpha_12 = 2 / (12 + 1)
     df = df.withColumn(
         "EMA_12",
@@ -25,7 +33,7 @@ def transform_data(df):
     )
 
     # RSI
-    print("➡️ Calculating RSI (14)")
+    print("Calculating RSI (14)")
     df = df.withColumn("prev_close", F.lag("close", 1).over(windowSpec))
     df = df.withColumn("change", F.col("close") - F.col("prev_close"))
     df = df.withColumn("gain", F.when(F.col("change") > 0, F.col("change")).otherwise(0))
@@ -47,84 +55,126 @@ def transform_data(df):
     )
 
     # Bollinger Bands
-    print("➡️ Calculating Bollinger Bands (20)")
+    print("Calculating Bollinger Bands (20)")
     df = df.withColumn("rolling_mean", F.avg("close").over(windowSpec.rowsBetween(-19, 0)))
     df = df.withColumn("rolling_std", F.stddev("close").over(windowSpec.rowsBetween(-19, 0)))
     df = df.withColumn("Bollinger_Upper", F.col("rolling_mean") + 2 * F.col("rolling_std"))
     df = df.withColumn("Bollinger_Lower", F.col("rolling_mean") - 2 * F.col("rolling_std"))
 
-    print("✅ Technical indicators calculation completed.")
+    print("Technical indicators calculation completed.")
     return df
 
-def read_silver_layer(spark, silver_path: str):
-    df = spark.read.format("delta").load(silver_path)
-    print(f"✅ Loaded Silver Layer data from {silver_path}")
-    return df
+def load_processed(df, output_path, last_date=None):
+    if last_date is None:
+        # Full load
+        df.write.format("delta") \
+            .mode("overwrite") \
+            .partitionBy("year", "month") \
+            .save(output_path)
+        print(f"✅ Delta table created (overwrite) at {output_path}")
+    else:
+        df_to_append = df.filter(F.col("date") > F.to_date(F.lit(last_date)))
 
-def load_processed(df, output_path: str):
-    (
-        df.write.format("delta")
-        .mode("overwrite")
-        .partitionBy("ticker", "year", "month")
-        .save(output_path)
-    )
-    print(f"✅ Saved Gold Layer data (partitioned) to {output_path}")
+        if df_to_append.count() > 0:
+            df_to_append.write.format("delta") \
+                .mode("append") \
+                .partitionBy("year", "month") \
+                .option("mergeSchema", "true") \
+                .save(output_path)
+            print(f"✅ Appended {df_to_append.count()} rows to Delta table at {output_path}")
+        else:
+            print("No new data to append")
+        
 
-def load_to_postgres(df):
-    #TODE: add ticker column
-    (
-        df.write.format("jdbc")
-        .option("url", "jdbc:postgresql://postgres:5432/stockdb")
-        .option("dbtable", "public.stock_gold")
-        .option("user", "your_user")
-        .option("password", "your_password")
-        .option("driver", "org.postgresql.Driver")
-        .mode("append")
-        .save()
+def load_to_postgres(df, last_date=None):
+    table_name = os.getenv("POSTGRES_TABLE", "public.stock_data")
+    url = os.getenv("POSTGRES_URL", "jdbc:postgresql://postgres:5432/stockdb")
+    user = os.getenv("POSTGRES_USER", "postgres")
+    password = os.getenv("POSTGRES_PASSWORD", "postgres")
+
+    if last_date is None:
+        df.write.format("jdbc") \
+            .option("url", url) \
+            .option("dbtable", table_name) \
+            .option("user", user) \
+            .option("password", password) \
+            .option("driver", "org.postgresql.Driver") \
+            .mode("overwrite") \
+            .save()
+        print(f"✅ Load all data to {table_name}")
+        return
+    else:
+        df_to_append = df.filter(F.col("date") > F.to_date(F.lit(last_date)))
+
+        if df_to_append.count() > 0:
+            df_to_append.write.format("jdbc") \
+                .option("url", url) \
+                .option("dbtable", table_name) \
+                .option("user", user) \
+                .option("password", password) \
+                .option("driver", "org.postgresql.Driver") \
+                .mode("append") \
+                .save()
+            print(f"✅ Appended {df_to_append.count()} rows to Postgres table {table_name}")
+        else:
+            print("No new data to append")
+
+def build_incremental(df_raw, df_processed, context_size=20):
+    # Get last processed date per ticker
+    last_date = df_processed.groupBy("ticker").agg(F.max("date").alias("last_date"))
+
+    new_data = (
+        df_raw.join(last_date, "ticker", "left")
+              .filter((F.col("last_date").isNull()) | (F.col("date") > F.col("last_date")))
     )
-    print("✅ Saved Gold Layer data to Postgres")
+
+    windowDesc = Window.partitionBy("ticker").orderBy(F.col("date").desc())
+
+    history_context = (
+        df_raw.join(last_date, "ticker", "inner")
+              .filter(F.col("date") <= F.col("last_date"))
+              .withColumn("rn", F.row_number().over(windowDesc))
+              .filter(F.col("rn") <= context_size)
+              .drop("rn")
+    )
+
+    return history_context.unionByName(new_data), last_date
 
 def transform_load_flow(
-    silver_path: str = "s3a://stock-data/raw/",
-    gold_path: str = "s3a://stock-data/processed/"
+    raw_path: str = "s3a://stock-data-lake/raw/",
+    processed_path: str = "s3a://stock-data-lake/processed/"
 ):
     spark = init_spark("StockTransform")
-
-    df_silver = read_silver_layer(spark, silver_path)
+    df_raw = extract_raw(spark, raw_path)
 
     try:
-        df_gold = spark.read.format("delta").load(gold_path)
+        df_raw = extract_raw(spark, raw_path)
+    
+        try:
+            df_processed = spark.read.format("delta").load(processed_path)
+            df_incremental, last_date = build_incremental(df_raw, df_processed)
+            df_transformed = transform_data(df_incremental)
+            df_final = (
+                df_transformed
+                .withColumn("year", F.col("date").substr(1, 4))
+                .withColumn("month", F.col("date").substr(6, 2))
+            )
+            
+            load_processed(df_final, processed_path, last_date)
+            load_to_postgres(df_final, last_date)        
+        except AnalysisException:
+            print("No processed found, running full transform...")
+            df_transformed = transform_data(df_raw)
+            df_final = (
+                df_transformed
+                .withColumn("year", F.col("date").substr(1, 4))
+                .withColumn("month", F.col("date").substr(6, 2))
+            )
 
-        last_gold = df_gold.groupBy("ticker").agg(F.max("date").alias("last_date"))
-
-        new_data = df_silver.join(last_gold, "ticker", "left") \
-            .filter((F.col("last_date").isNull()) | (F.col("date") > F.col("last_date")))
-
-        windowSpec = Window.partitionBy("ticker").orderBy(F.col("date").desc())
-        history_context = df_silver.join(last_gold, "ticker", "inner") \
-            .filter(F.col("date") <= F.col("last_date")) \
-            .withColumn("rn", F.row_number().over(windowSpec)) \
-            .filter(F.col("rn") <= 20) \
-            .drop("rn")
-
-        df_incremental = history_context.unionByName(new_data)
-
-        df_transformed = transform_data(df_incremental)
-
-    except AnalysisException:
-        print("No Gold layer found, running full transform...")
-        df_transformed = transform_data(df_silver)
-
-    df_final = (
-        df_transformed
-        .withColumn("year", F.col("date").substr(1, 4))
-        .withColumn("month", F.col("date").substr(6, 2))
-    )
-
-    load_processed(df_final, gold_path, mode="append")
-    load_to_postgres(df_final)
-
-    spark.stop()
+            load_processed(df_final, processed_path)
+            load_to_postgres(df_final)
+    finally:
+        spark.stop()
 
 if __name__ == "__main__":
     transform_load_flow()
